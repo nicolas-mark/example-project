a9553d26b7b76853378468be3c04184b265ee814
Nick <nmark@cogility.com>
2024-03-27 22:52:16 -0400

    feat(auth): auth elixir project

---
+++ auth/lib/web_authn.ex
@@ -0,0 +1,18 @@
+defmodule WebAuthn do
+  @moduledoc """
+  Documentation for `WebAuthn`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WebAuthn.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
